123a82e17b8e11bfaca589d13a524f7302b341f5
TreeKR <johanbag290@gmail.com>
2021-01-12 22:02:28 +0900

    change

--- src/Electron.js
+++ src/Electron.js
@@ -41,7 +41,7 @@ function create_window() {
     }
     else {
         // 프로덕션 환경에서는 패키지 내부 리소스에 접근
-        main_window.loadFile(path.join(__dirname, './build/index.html'));
+        main_window.loadFile(path.join(__dirname, '../build/index.html'));
     }
     // Emitted when the window is closed.
 }
