825e3be973e527b99f813a2c070f4b24f21069d4
TreeKR <johanbag290@gmail.com>
2021-01-16 18:44:20 +0900

    vsc run & scene change

--- src/login.tsx
+++ src/login.tsx
@@ -6,14 +6,13 @@ import logo from './icon/Abins-world.png';
 import styles from './App.module.css';
 import Authenticator from './util/Authenticate';
 import { Link } from 'react-router-dom';
-import { HashHistory } from 'history';
 
 interface State {
   email: String,
   password: String
 }
 
-class Login extends Component<{history: HashHistory}, State> {
+class Login extends Component<{history: any}, State> {
 
   buttonRef: any
 
@@ -121,7 +120,7 @@ class Login extends Component<{history: HashHistory}, State> {
             rel="noopener noreferrer">
             비밀번호를 잊으셨나요?
           </a>
-            <a
+          <a
             className={styles.passwordlink}
             href="https://signup.live.com/signup?ru=https%3a%2f%2flogin.live.com%2foauth20_authorize.srf%3flc%3d2066%26redirect_uri%3dhttps%3a%252f%252fsisu.xboxlive.com%252fconnect%252foauth%252fXboxLive%26response_type%3dcode%26state%3dLAAAAAEB1ufJ-SJRurM1pEgCt7g4ZoV_faB9gfe4CDuRFQgW2higKAGC4-SWMWUzZWQ4Y2I1YWE0NDViMDk5MWI2MDBlMDJkZDg3MjEx%26client_id%3d000000004420578E%26scope%3dXboxLive.Signin%26lw%3d1%26fl%3ddob%2ceasi2%26xsup%3d1%26cobrandid%3d8058f65d-ce06-4c30-9559-473c9275a65d%26mkt%3dKO-KR%26uaid%3d917ed2f1993144599f260777fbcda0a9&mkt=KO-KR&uiflavor=web&lw=1&fl=dob%2ceasi2&cobrandid=8058f65d-ce06-4c30-9559-473c9275a65d&client_id=000000004420578E&uaid=917ed2f1993144599f260777fbcda0a9&suc=000000004420578E&lic=1"
             target="_blank"
