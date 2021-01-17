4ba871791a35719285ef6cc1129a7986041d48e4
TreeKR <johanbag290@gmail.com>
2021-01-17 21:30:33 +0900

    프로필 추가(CSS 변경 필요)

--- bootstraper/Electron.ts
+++ bootstraper/Electron.ts
@@ -38,7 +38,7 @@ function create_window() {
   if (isDev) {
     // 개발 중에는 개발 도구에서 호스팅하는 주소에서 로드
     main_window.loadURL('http://localhost:3000')
-
+    main_window.webContents.openDevTools()
   } else {
     // 프로덕션 환경에서는 패키지 내부 리소스에 접근
     main_window.loadFile(path.join(__dirname, '../build/index.html'))
--- src/main.tsx
+++ src/main.tsx
@@ -26,7 +26,24 @@ class App extends Component<{history: any}> {
                 </div>
                     <div className={styles.servers}>
                         <div className={styles.state}>
+
+
+
                             <input style={play}className={styles.play} type="button" name="play" value="플레이"></input>
+
+                            <div className={styles.profile}>
+                              <table>
+                                <tr>
+                                  <td rowSpan={2}><img src="https://minotar.net/avatar/f93bb85a694c4eb7a35ae080c0b1eb21" height="84" width="84"></img></td>
+                                </tr>
+                                <tr>
+                                  <td>test</td>
+                                </tr>
+                                <tr>
+                                <td>test</td>
+                                </tr>
+                              </table>
+                            </div>
 
                         </div>
                         <div>
